ed660749adb2aadadfc434988270064eb9f60637
Tarun Chaudhary <tarun.chaudhary@lybrate.com>
2022-11-30 09:12:46 +0530

    Initial Commit

---
+++ LoginProject/LoginProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginProjectApp.swift
+//  LoginProject
+//
+//  Created by Tarun Chaudhary on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
